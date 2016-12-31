92b11edc344a4ded73f53e17d57b55fd03f3eff5
BerntAnker <bernt.anker@me.com>
2016-12-31 02:13:15 +0100

    added distance calculation

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 let config = require('./config');
 let logger = require("./lib/logger");
-let addAisPositions = require('./lib/ais/mongoose-repository/add-ais-positions');
-let getAisPositions = require("./lib/ais/mongoose-repository/get-ais-positions");
-let removeAisPositions = require('./lib/ais/mongoose-repository/remove-ais-positions');
-let fetchAisData = require('./lib/ais/fetch-ais-data');
-
+let ais = require('./lib/ais');
 
 let init = function () {
 
     let importData = function () {
-        fetchAisData(config.aisDataUrl)
-            .then(aisData => addAisPositions(aisData)
+        ais.fetchAisData(config.aisDataUrl)
+            .then(aisData => ais.repository.addAisPositions(aisData)
                 .then(data => logger.debug(`All new ais positions written to database. (${data.length})`))
             )
             .catch(error =>{logger.error(`Fetching ais data from ${config.aisDataUrl} failed! ${error}`)});
     };
+
     setInterval(importData,config.fetchDataInterval);
 };
 
 init();
 
-getAisPositions("2016-12-29T09:58:00", "2016-12-31T14:00:00")
-    .then(aisData=>removeAisPositions(aisData))
+ais.repository.getAisPositions("2016-12-29T09:58:00", "2016-12-31T14:00:00",257654700)
+    //.then(aisData=>removeAisPositions(aisData))
+    .then(aisData=>console.log('Tidsrom: 2016-12-29T09:58:00 - 2016-12-31T14:00:00 mmsi: 257654700 - distanse(nm):',ais.repository.getDistance(aisData)))
     .catch(error=>logger.error(error));
+
