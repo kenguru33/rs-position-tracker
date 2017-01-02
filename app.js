0d580a33e351458ee4eac4f16de1569dc4ec85b3
BerntAnker <bernt.anker@me.com>
2017-01-02 22:33:35 +0100

    Cleanup

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ let init = function () {
                 .then(data => logger.debug(`All new ais positions written to database. (${data.length})`))
             )
             .catch(error =>{logger.error(`Fetching ais data from ${config.aisDataUrl} failed! ${error}`)});
+
+        ais.repository.getAisPositions("2016-12-29T09:58:00", "2017-01-02T23:00:00",258144000)
+        //.then(aisData=>removeAisPositions(aisData))
+            .then(aisData=>console.log(aisData,ais.repository.getDistance(aisData)))
+            .catch(error=>logger.error(error));
     };
 
     setInterval(importData,config.fetchDataInterval);
@@ -17,7 +22,7 @@ let init = function () {
 
 init();
 
-ais.repository.getAisPositions("2016-12-29T09:58:00", "2016-12-31T14:00:00",257654700)
+ais.repository.getAisPositions("2016-12-29T09:58:00", "2017-01-02T23:00:00",258144000)
     //.then(aisData=>removeAisPositions(aisData))
     .then(aisData=>console.log('Tidsrom: 2016-12-29T09:58:00 - 2016-12-31T14:00:00 mmsi: 257654700 - distanse(nm):',ais.repository.getDistance(aisData)))
     .catch(error=>logger.error(error));
