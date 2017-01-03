36707a5523970e5704e5e2a46ddfac030e905143
Bernt Anker <bernt.anker@me.com>
2017-01-03 22:03:17 +0100

    Added webservice and made some adjustments to repo

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
 let config = require('./config');
 let logger = require("./lib/logger");
 let ais = require('./lib/ais');
+const chalk = require('chalk');
+const express = require('express');
 
-let init = function () {
 
-    let importData = function () {
-        ais.fetchAisData(config.aisDataUrl)
-            .then(aisData => ais.repository.addAisPositions(aisData)
-                .then(data => logger.debug(`All new ais positions written to database. (${data.length})`))
-            )
-            .catch(error =>{logger.error(`Fetching ais data from ${config.aisDataUrl} failed! ${error}`)});
+// init ais Import Service
 
-        ais.repository.getAisPositions("2016-12-29T09:58:00", "2017-01-02T23:00:00",258144000)
-        //.then(aisData=>removeAisPositions(aisData))
-            .then(aisData=>console.log("distanse: ",ais.repository.getDistance(aisData) + " " + aisData.length))
-            .catch(error=>logger.error(error));
-    };
+let importData = function () {
+    ais.fetchAisData(config.aisDataUrl)
+        .then(aisData => ais.repository.addAisPositions(aisData)
+            .then(data => logger.debug(`All new ais positions written to database. (${data.length})`))
+        )
+        .catch(error =>{logger.error(`Fetching ais data from ${config.aisDataUrl} failed! ${error}`)});
 
-    setInterval(importData,config.fetchDataInterval);
+    /*ais.repository.getAisPositions("2016-12-29T09:58:00", "2017-01-04T23:00:00",257013400)
+     //.then(aisData=>removeAisPositions(aisData))
+     .then(aisData=>console.log("distanse: ",ais.repository.getDistance(aisData) + " " + aisData.length))
+     .catch(error=>logger.error(error.stack));*/
 };
 
-init();
+setInterval(importData,config.fetchDataInterval);
 
-ais.repository.getAisPositions("2016-12-29T09:58:00", "2017-01-02T23:00:00",258144000)
-    //.then(aisData=>removeAisPositions(aisData))
-    .then(aisData=>console.log('Tidsrom: 2016-12-29T09:58:00 - 2016-12-31T14:00:00 mmsi: 257654700 - distanse(nm):',ais.repository.getDistance(aisData)))
-    .catch(error=>logger.error(error));
+console.log(chalk.green("Ais Import Service started with fetch interval=" +config.fetchDataInterval)+ "(ms)");
+
+
+// init express application
+
+/**
+ * Serving ais position data. mmsi=* -> all vessels
+ *
+ */
+let app = express();
+
+/**
+ * Gets ais positions for vessel in time interval.
+ */
+app.get('/positions/:mmsi/:fromTime/:toTime', function (req, res) {
+
+    if (req.params.mmsi=='*') {
+        req.params.mmsi = null;
+    }
+
+    ais.repository.getAisPositions(req.params.fromTime,req.params.toTime,req.params.mmsi)
+        .then(aisData => {
+            res.send(aisData);
+        })
+        .catch(error=>{
+            logger.error(error.stack);
+            res.send(JSON.stringify(error.message));
+        });
+});
+
+app.get('/distance/:mmsi/:fromTime/:toTime', function (req, res) {
+
+    ais.repository.getAisPositions(req.params.fromTime, req.params.toTime, req.params.mmsi)
+     .then(aisData=>{
+         distance = ais.repository.getDistance(aisData);
+
+         res.send({
+             MMSI: req.params.mmsi,
+             FromLocalTime: req.params.fromTime,
+             ToLocalTime: req.params.toTime,
+             PositionCount: aisData.length,
+             Distance_nm: distance*0.539956803,
+             Distance_km: distance
+         });
+
+     })
+     .catch(error=>{
+         logger.error(error.stack);
+         res.send(JSON.stringify(error.message));
+     });
+});
+
+app.listen(3000, function () {
+    console.log(chalk.green("Web Service started on port 3000"));
+});
+
 
