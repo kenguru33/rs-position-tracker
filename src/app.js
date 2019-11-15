54d82d241c98b0568a0c86d71a1357ceaca2936e
Bernt Anker <bernt.anker@me.com>
2019-11-15 22:15:39 +0100

    Implemented swagger

--- src/app.js
+++ src/app.js
@@ -10,16 +10,37 @@ const apiRouter = require('./routes/api-router')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'RS Position Tracker', // Title (required)
+      version: '1.0.0' // Version (required)
+    }
+  },
+  // Path to the API docs
+  apis: ['src/routes/api-router.js']
+}
+
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJsDoc(options)
+console.log(swaggerSpec)
 // init mongoose default connection
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.DB_URI).then(conn => {
-  console.log(chalk.green('Connection to database established.'))
-}).catch(err => {
-  console.log(chalk.red(`Error: ${err.message}`))
-  console.log(chalk.red('Shutting down...'))
-  process.exit(1)
-})
+mongoose
+  .connect(process.env.DB_URI)
+  .then(conn => {
+    console.log(chalk.green('Connection to database established.'))
+  })
+  .catch(err => {
+    console.log(chalk.red(`Error: ${err.message}`))
+    console.log(chalk.red('Shutting down...'))
+    process.exit(1)
+  })
 
 // init ais Import Service
 const importData = function () {
@@ -57,6 +78,13 @@ if (process.env.ENABLE_API) {
   app.use(morgan('combined'))
 
   app.use('/api', apiRouter)
+
+  // serve swagger
+  app.get('/swagger.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+  })
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
   app.use('/', express.static(path.join(__dirname, '/public')))
 
--- src/routes/api-router.js
+++ src/routes/api-router.js
@@ -5,14 +5,201 @@ const stationController = require('../controllers/stationController.js')
 
 const api = express.Router()
 
+/**
+ * @swagger
+ *
+ * /api/get_positions/{mmsi}/{fromTime}/{toTime}:
+ *   get:
+ *     description: Get Vessel posistions by mmsi and timespan (use zulu time)
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: string
+ *       - name: fromTime
+ *         description: Start of timespan (YYYY-MM-DDTHH:MM:SS)
+ *         in: path
+ *         required: true
+ *         example: 2019-11-15T09:45:00
+ *         type: string
+ *       - name: toTime
+ *         description: End of timespan (YYYY-MM-DDTHH:MM:SS)
+ *         in: path
+ *         required: true
+ *         example: 2019-11-15T23:00:00
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Ais Data
+ */
 api.get('/get_positions/:mmsi/:fromTime/:toTime', aisController.getPositions)
+
+/**
+ * @swagger
+ *
+ * /api/get_distance/{mmsi}/{fromTime}/{toTime}:
+ *   get:
+ *     description: Get traveled distance in a given period
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: string
+ *       - name: fromTime
+ *         description: Start of timespan (YYYY-MM-DDTHH:MM:SS)
+ *         in: path
+ *         required: true
+ *         example: 2019-11-15T09:45:00
+ *         type: string
+ *       - name: toTime
+ *         description: End of timespan (YYYY-MM-DDTHH:MM:SS)
+ *         in: path
+ *         required: true
+ *         example: 2019-11-15T23:00:00
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Distance
+ */
 api.get('/get_distance/:mmsi/:fromTime/:toTime', aisController.getDistance)
-api.get('/get_vessel/:rsid', vesselController.getVessel)
+
+/**
+ * @swagger
+ *
+ * /api/get_moving_vessels:
+ *   get:
+ *     description: Get vessels that are one the move at htis moment
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Moving Vessels
+ */
 api.get('/get_moving_vessels', vesselController.getMovingVessels)
-api.get('/get_station/:name', stationController.getStation)
+
+/**
+ * @swagger
+ *
+ * /api/get_TTG/{mmsi}/{longitude}/{latitude}:
+ *   get:
+ *     description: Time To Go
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: number
+ *       - name: latitude
+ *         description: Latitude (decimal)
+ *         in: path
+ *         required: true
+ *         example: 68
+ *         type: number
+ *       - name: longitude
+ *         description: Longitude (devimal)
+ *         in: path
+ *         required: true
+ *         example: 12
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Distance
+ */
 api.get('/get_TTG/:mmsi/:latitude/:longitude', aisController.getTTG)
-api.get('/get_ETA/:latitude/:longitude', aisController.getETA)
+
+/**
+ * @swagger
+ *
+ * /api/get_ETA/{mmsi}/{longitude}/{latitude}:
+ *   get:
+ *     description: Estimated Time at Arival
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: number
+ *       - name: latitude
+ *         description: Latitude (decimal)
+ *         in: path
+ *         required: true
+ *         example: 68
+ *         type: number
+ *       - name: longitude
+ *         description: Longitude (devimal)
+ *         in: path
+ *         required: true
+ *         example: 12
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Estimated Time at Arival
+ */
+
+/**
+ * @swagger
+ *
+ * /api/get_last_position/{mmsi}:
+ *   get:
+ *     description: Get last registrated position
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Get last registrated position
+ */
 api.get('/get_last_position/:mmsi', aisController.getLastPosition)
+
+/**
+ * @swagger
+ *
+ * /api/get_position/{mmsi}/{time}:
+ *   get:
+ *     description: Get position at a given time
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: mmsi
+ *         description: Maritime Mobile Service Identity
+ *         in: path
+ *         required: true
+ *         example: 259460000
+ *         type: string
+ *       - name: time
+ *         description: Date (YYYY-MM-DDTHH:MM:SS)
+ *         in: path
+ *         required: true
+ *         example: 2019-11-15T09:45:00
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Vessel position at a given time. Position is returned if found in defined timespan +/- from given time. (Timespan is set in app config)
+ */
 api.get('/get_position/:mmsi/:time', aisController.getPosition)
+
+api.get('/get_ETA/:latitude/:longitude', aisController.getETA)
+api.get('/get_vessel/:rsid', vesselController.getVessel)
+api.get('/get_station/:name', stationController.getStation)
 
 module.exports = api
